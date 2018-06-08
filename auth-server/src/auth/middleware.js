256abcc2c9e74e70c7b9b852f52acd1eec83204a
Ovidiu Parasca <OviParasca@gmail.com>
2018-06-08 15:46:29 -0700

    - updated the files for lab-17

--- auth-server/src/auth/middleware.js
+++ auth-server/src/auth/middleware.js
@@ -4,10 +4,6 @@ import User from './model.js';
 
 export default (req, res, next) => {
 
-<<<<<<< HEAD
-  let authorize = (token) => {
-    User.authorize(token)
-=======
 
   let authorize = (token) => {
   
@@ -25,35 +21,10 @@ export default (req, res, next) => {
 
   let authenticate = (auth) => {
     User.authenticate(auth)
->>>>>>> b2047eff4e6be98af37c016ea965d394777a7492
       .then(user => {
         if (!user) {
           getAuth();
-        } else {
-          next();
         }
-<<<<<<< HEAD
-      }).catch(next);
-  };
-
-  let getAuth = () => {
-    next({
-      status:401,
-      statusMessage: 'Unauthorized',
-      message: 'Invalid User Credentials',
-    });
-  };
-
-  let authenticate = (auth) => {
-    User.authenticate(auth).then(user => {
-      if (!user) {
-        getAuth();
-      } else {
-        req.token = user.generateToken();
-        next();
-      }
-    }).catch(next);
-=======
         req.token = user.generateToken();
         // req.user = user;
         next();
@@ -64,7 +35,6 @@ export default (req, res, next) => {
     // res.set({
     //   'WWW-Authenticate': 'Basic realm="protected secret stuff"',
     // }).send(401);
->>>>>>> b2047eff4e6be98af37c016ea965d394777a7492
   };
 
   try {
@@ -72,7 +42,7 @@ export default (req, res, next) => {
     let authHeader = req.headers.authorization;
 
     if (!authHeader) {
-      return getAuth();
+      getAuth();
     }
 
     if (authHeader.match(/basic/i)) {
@@ -80,20 +50,12 @@ export default (req, res, next) => {
       let base64Buf = new Buffer(base64Header, 'base64');
       let [username, password] = base64Buf.toString().split(':');
       auth = {username, password};
-<<<<<<< HEAD
-      authenticate(auth);
-    } 
-    else if (authHeader.match(/bearer/i)) {
-      let token = authHeader.replace(/Bearer\s+/, '');
-      authorize(token);
-=======
       authenticate(auth, next);
     } else if (authHeader.match(/bearer/i)) {
       let token = authHeader.replace(/Bearer\s+/i, '');
       authorize(token);
       // auth.token = authHeader.replace(/bearer\s+/, '');
       // authenticate(auth, next);
->>>>>>> b2047eff4e6be98af37c016ea965d394777a7492
     }
   } catch(err) {
     next(err);
--- auth-server/src/auth/router.js
+++ auth-server/src/auth/router.js
@@ -5,12 +5,6 @@ const authRouter = express.Router();
 
 import User from './model.js';
 import auth from './middleware.js';
-<<<<<<< HEAD
-// import { userInfo } from 'os';
-=======
-// authRouter.param('user', User);
->>>>>>> b2047eff4e6be98af37c016ea965d394777a7492
-
 
 authRouter.post('/api/v1/signup', (req, res, next) => {
   if (!req.body.username || !req.body.password || !req.body.email) {
@@ -24,35 +18,6 @@ authRouter.post('/api/v1/signup', (req, res, next) => {
 });
 
 authRouter.get('/api/v1/signin', auth, (req, res, next) => {
-<<<<<<< HEAD
-  res.cookie('Token', req.token);
-  res.send(req.token);
-  next();
-});
-
-authRouter.get('/api/v1/userprofile', auth, (req, res, next) => {
-  res.send('user access');
-  next();
-});
-
-authRouter.post('/api/v1/user', auth, (req, res, next) => {
-  res.send(res.user).then().catch(next);
-});
-
-// authRouter.get('/api/v1/user/:Id', auth (req, res, next) => {
-//   console.log(`user Id: ${user}`);
-// });
-
-// authRouter.put('/api/v1/user/:Id', auth (req, res, next) => {
-//   console.log(`user Id: ${user}`);
-// });
-
-// authRouter.delete('/api/v1/user/:Id', auth (req, res, next) => {
-//   console.log(`user Id: ${user}`);
-// });
-
-export default authRouter;
-=======
   console.log(`\nToken: ${req.token}`);
   res.cookie('Token', req.token);
   res.send(req.token);
@@ -69,17 +34,6 @@ authRouter.get('/api/v1/users/:id', (req, res, next) => {
   .then(data => { sendJSON(res,  data)})
   .catch(next);
 });
-
-// authRouter.post('/api/v1/show', auth, (req, res, next) => {
-  // let user = new User(req.body);
-  // res.send(user);
-// });
-
-// authRouter.post('/api/v1/getme', auth, (req, res, next) => {
-//   let user = new User(req.body);
-//   req.user.findOne();
-//   res.send(user);
-// });
 
 
 // authRouter.delete('/api/v1/:id', (req,res, next) => {
@@ -98,4 +52,3 @@ let sendJSON = (res, data) => {
 };
 
 export default authRouter;
->>>>>>> b2047eff4e6be98af37c016ea965d394777a7492
